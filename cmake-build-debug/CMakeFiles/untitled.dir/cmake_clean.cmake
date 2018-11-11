676e31bb8eb3e5201a35492524ce8a2ea192e6e8
y0natancohen <yonayonatan@gmail.com>
2018-11-11 20:59:22 +0200

    sdfsdf

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/src/Main.cpp.o"
+  "CMakeFiles/untitled.dir/src/Action.cpp.o"
+  "CMakeFiles/untitled.dir/src/Customer.cpp.o"
+  "CMakeFiles/untitled.dir/src/Dish.cpp.o"
+  "CMakeFiles/untitled.dir/src/Restaurant.cpp.o"
+  "CMakeFiles/untitled.dir/src/Table.cpp.o"
+  "CMakeFiles/untitled.dir/src/SortableDish.cpp.o"
+  "untitled.pdb"
+  "untitled"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
